05779364531850ce473ab44a80b9d459e403689e
Muralidhar-3 <69751655+Muralidhar-3@users.noreply.github.com>
2022-11-05 01:30:43 +0530

    Add files via upload
    initial commit

---
+++ server/src/schema/stateHandlerSchema.ts
@@ -0,0 +1,58 @@
+import { Schema, type, MapSchema } from "@colyseus/schema";
+
+import {
+  PlayerCrouchSchema,
+  PlayerDirectionSchema,
+  PlayerFileNameSchema,
+  PlayerKeySchema,
+  PlayerPositionSchema,
+  PlayerSchema,
+} from "./playerSchema";
+
+export class StateHandlerSchema extends Schema {
+  @type({ map: PlayerSchema })
+  players = new MapSchema<PlayerSchema>();
+
+  addPlayer(sessionId: string) {
+    this.players.set(
+      sessionId,
+      new PlayerSchema().assign({ sessionId: sessionId })
+    );
+  }
+
+  getPlayer(sessionId: string): PlayerSchema {
+    return this.players.get(sessionId);
+  }
+
+  removePlayer(sessionId: string) {
+    this.players.delete(sessionId);
+  }
+
+  setDirection(sessionId: string, direction: PlayerDirectionSchema) {
+    this.getPlayer(sessionId).playerDirection.rotationY = direction.rotationY;
+  }
+
+  setKeys(sessionId: string, keys: PlayerKeySchema) {
+    const player = this.getPlayer(sessionId);
+    player.playerKey.up = keys.up;
+    player.playerKey.right = keys.right;
+    player.playerKey.down = keys.down;
+    player.playerKey.left = keys.left;
+    player.playerKey.wave = keys.wave;
+  }
+
+  setPosition(sessionId: string, position: PlayerPositionSchema) {
+    const player = this.getPlayer(sessionId);
+    player.playerPosition.x = position.x;
+    player.playerPosition.y = position.y;
+    player.playerPosition.z = position.z;
+  }
+
+  setCrouching(sessionId: string, crouchSchema: PlayerCrouchSchema) {
+    this.getPlayer(sessionId).playerCrouch.crouching = crouchSchema.crouching;
+  }
+
+  setFileName(sessionId: string, playerFileName: PlayerFileNameSchema) {
+    this.getPlayer(sessionId).playerFileName.fileName = playerFileName.fileName;
+  }
+}
